962cef7a75a1915f46c1bc9ea5597c3403451e0a
Mefteg <gimenez.tom@gmail.com>
2014-07-22 17:15:27 +0200

    Work on Player Behaviour. Work on level1

--- assets/behaviours/player.js
+++ assets/behaviours/player.js
@@ -4,31 +4,30 @@
 
 var Player = function(_gameobject) {
 	LR.Behaviour.call(this, _gameobject);
-
-  this.speed = 60;
-
-	this.cursors = this.go.game.input.keyboard.createCursorKeys();
 };
 
 Player.prototype = Object.create(LR.Behaviour.prototype);
 Player.prototype.constructor = Player;
 
+Player.prototype.start = function() {
+  // set max speed
+  this.speed = new Phaser.Point(150, 60);
+  // set no gravity
+  this.go.entity.body.data.gravityScale = 0;
+
+  // set camera to follow Dude
+  this.go.game.camera.follow(this.go.entity);
+
+  // enable cursors
+  this.cursors = this.go.game.input.keyboard.createCursorKeys();
+};
+
 Player.prototype.update = function() {
-	if (this.cursors.left.isDown) {
-  	this.go.body.moveLeft(this.speed);
-
-  	if (this.go.entity.scale.x < 0) {
-  		this.go.entity.scale.x = 1;
-  	}
-  } else if (this.cursors.right.isDown) {
-  	this.go.body.moveRight(this.speed);
-
-  	if (this.go.entity.scale.x > 0) {
-  		this.go.entity.scale.x = -1;
-  	}
-  }
+	this.go.body.moveRight(this.speed.x)
 
   if (this.cursors.up.isDown) {
-  	this.go.body.moveUp(200);
+  	this.go.body.moveUp(this.speed.y);
+  } else if (this.cursors.down.isDown) {
+    this.go.body.moveDown(this.speed.y);
   }
-}
+};
